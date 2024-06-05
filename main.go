25be3494ed7d6b9d568d19da29ff03fedf87107b
MGavranovic <mgavranovic7@gmail.com>
2024-06-05 13:52:21 +0200

    slices exercise

--- main.go
+++ main.go
@@ -450,3 +450,45 @@ func main() {
 }
 
 */
+
+// NOTE: Exercise: Slices
+/*
+Implement Pic. It should return a slice of length dy, each element of which is a slice of dx 8-bit unsigned integers. When you run the program, it will display your picture, interpreting the integers as grayscale (well, bluescale) values.
+
+The choice of image is up to you. Interesting functions include (x+y)/2, x*y, and x^y.
+
+(You need to use a loop to allocate each []uint8 inside the [][]uint8.)
+
+(Use uint8(intValue) to convert between types.)
+
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	//"fmt"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+	// dy being the length
+	pic := make([][]uint8, dy)
+	
+	// iterate over pic
+	for y := 0; y < dy; y++{
+		// at every iteration create a row (slice)
+		row := make([]uint8, dx)
+		for x := 0; x < dx; x++ {
+			// than iterate over each row assigning value for each element in that row(slice)
+			row[x] = uint8((x+y)/2)
+		}
+	pic[y] = row
+	//fmt.Println(pic[y])
+	}
+	// returning the slice of length dy
+	return pic
+}
+
+func main() {
+	pic.Show(Pic)
+}
+
+*/
